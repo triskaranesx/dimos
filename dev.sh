61b50efe512ecbb306043992206f13225c3fbc4a
lesh <lesh@sysphere.org>
2025-04-16 14:28:04 -0700

    auto build dev img if missing
    Former-commit-id: e7187ee432d281d3bfc7a4ce39b057b4c6564469

--- dev.sh
+++ dev.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-if [ "$1" == "build" ]; then
+build_image() {
     docker rm -f dimos-dev
     docker build \
         --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) \
         --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
         -t dimensionalos/dev-base docker/dev/base/
+}
+
+if [ "$1" == "build" ]; then
+    build_image
+else
+    # Check if image exists
+    if ! docker image inspect dimensionalos/dev-base &>/dev/null; then
+        echo "Image dimensionalos/dev-base not found. Building..."
+        build_image
+    fi
 fi
 
 docker-compose -f docker/dev/base/docker-compose.yaml up -d && docker exec -it dimos-dev bash
