4944d8a0de6518747497afc481951f4245eef579
stash <pomichterstash@gmail.com>
2025-12-11 03:17:37 +0000

    Test revert spatial memory test - CI hanging
    Former-commit-id: 4cd055b445127fcbbea45948377d63a86c9d193b [formerly 91458013221a212340c3e4c9b0468c6be44f6394]
    Former-commit-id: 3c063b634550b7a960c4a187b61ce76252a35a2e

--- dimos/perception/test_spatial_memory.py
+++ dimos/perception/test_spatial_memory.py
@@ -25,7 +25,7 @@ from reactivex import Observable
 from reactivex import operators as ops
 from reactivex.subject import Subject
 
-from dimos.msgs.geometry_msgs import Pose, Vector3
+from dimos.msgs.geometry_msgs import Pose
 from dimos.perception.spatial_perception import SpatialMemory
 from dimos.stream.video_provider import VideoProvider
 
@@ -124,7 +124,7 @@ class TestSpatialMemory:
                 return {
                     "frame": frame,
                     "position": transform["position"],
-                    "rotation": Vector3(0, 0, 0),  # Using zero rotation for testing
+                    "rotation": transform["position"],  # Using position as rotation for testing
                 }
 
             # Create a stream that processes each frame
