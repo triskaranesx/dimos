2779405503d842785eeb11344acc507e319435cd
lesh <lesh@sysphere.org>
2025-12-21 07:17:41 +0000

    rosnav spec
    Former-commit-id: 8dabd5644b9be9482bc36c22bf91cf09eec481ec [formerly 7823d7808e0335a0d5f9c7e13e78af617adb514c]
    Former-commit-id: 49d0f6f795ee9ae369fc1642b701ae8ac5aa6348

---
+++ dimos/navigation/rosnav.py
@@ -0,0 +1,40 @@
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dimos.core import In, Module, Out
+from dimos.msgs.geometry_msgs import Path, PoseStamped, Twist
+from dimos.msgs.sensor_msgs import PointCloud2
+from dimos.navigation.spec import NavSpec
+from dimos.perception.pointcloud.spec import GlobalPointcloudPerception, PointcloudPerception
+
+
+class Config:
+    global_frame_id: str = "world"
+
+
+class RosNav(Module, PointcloudPerception, GlobalPointcloudPerception, NavSpec):
+    goal_req: In[PoseStamped] = None  # type: ignore
+    goal_active: Out[PoseStamped] = None  # type: ignore
+    path_active: Out[Path] = None  # type: ignore
+
+    ctrl: Out[Twist] = None  # type: ignore
+
+    pointcloud: Out[PointCloud2] = None  # type: ignore
+    global_pointcloud: Out[PointCloud2] = None  # type: ignore
+
+    config: Config
+
+    def __init__(self, *args, **kwargs):
+        self.config = Config(**kwargs)
+        super().__init__()
